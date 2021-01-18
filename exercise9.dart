0876efe154a1f7620ec860330b39d1960d538110
Hafsah2020 <60625866+Hafsah2020@users.noreply.github.com>
2021-01-18 18:05:33 +0100

    Update exercise9.dart

--- exercise9.dart
+++ exercise9.dart
@@ -5,6 +5,28 @@
  * Keep track of how many guesses the user has taken, and when the game ends, print this out.
  */
 
+import 'dart:io';
+import 'dart:math';
+
 main(List<String> args) {
-  
+  int max = 100;
+  int min = 1;
+  Random a = new Random();
+  int number = min + a.nextInt(max - min);
+  int guess = 0;
+  int i = 0;
+  while (guess != number) {
+    print('Number:- ');
+    int guess = int.parse(stdin.readLineSync());
+    i++;
+    if (guess > number) {
+      print('Too high');
+    } else if (guess == number) {
+      print('right');
+      break;
+    } else {
+      print('Too low');
+    }
+  }
+  print('Number of guesses:- ${i}');
 }
