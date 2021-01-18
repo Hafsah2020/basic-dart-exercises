9a4e31b2b8d9444896f24e70de914a80d189c6bc
Hafsah2020 <60625866+Hafsah2020@users.noreply.github.com>
2021-01-18 18:04:02 +0100

    Update exercise6.dart

--- exercise6.dart
+++ exercise6.dart
@@ -4,6 +4,18 @@
  * A palindrome is a string that reads the same forwards and backwards.
  */
 
+import 'dart:io';
+
 main(List<String> args) {
-  
+  print('word:- ');
+  String word = stdin.readLineSync();
+  var a = word.split('').join('');
+  var b = word.split('').reversed.join('');
+  if (a == b) {
+    print('it\'s a palindrome');
+  } else {
+    print('it\'s not a palindrome');
+  }
+  print(a);
+  print(b);
 }
