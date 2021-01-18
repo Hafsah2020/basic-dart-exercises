dd9882953e56d545c1ec9f1b8dadae521e84f6d4
Hafsah2020 <60625866+Hafsah2020@users.noreply.github.com>
2021-01-18 15:11:23 +0100

    Update exercise2.dart

--- exercise2.dart
+++ exercise2.dart
@@ -4,6 +4,14 @@
  * Depending on whether the number is even or odd, print out an appropriate message to the user.
  */
 
+import 'dart:io';
+
 main(List<String> args) {
-  
+  print('Number:- ');
+  int number = int.parse(stdin.readLineSync());
+  if ((number % 2) == 0) {
+    print('It\'s even');
+  } else {
+    print(('not even'));
+  }
 }
