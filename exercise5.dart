8c1d0d5dc75635e221826c4125a3b90cf09511f1
Hafsah2020 <60625866+Hafsah2020@users.noreply.github.com>
2021-01-18 15:13:55 +0100

    Update exercise5.dart

--- exercise5.dart
+++ exercise5.dart
@@ -7,6 +7,17 @@
  * (without duplicates). Make sure your program works on two lists of different sizes.
  */
 
+import 'dart:core';
+
 main(List<String> args) {
-  
+  var a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  var b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+  var c = new List();
+  a.forEach((element) {
+    if (b.contains(element)) {
+      c.add(element);
+    }
+  });
+  var distinctlist = c.toSet().toList();
+  print(distinctlist);
 }
