f1301fc5a2436e326116e4a3f1439176e2c1b280
Hafsah2020 <60625866+Hafsah2020@users.noreply.github.com>
2021-01-18 15:15:22 +0100

    Update exercise4.dart

--- exercise4.dart
+++ exercise4.dart
@@ -1,5 +1,5 @@
 /**
- * Exercise 1
+ * Exercise 4
  * Create a program that asks the user for a number and then prints out a 
  * list of all the divisors of that number.
  * If you don’t know what a divisor is, it is a number that divides evenly into another number.
