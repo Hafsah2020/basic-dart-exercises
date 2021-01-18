76d599be861c009c1061b55dc157cea13e712d60
Hafsah2020 <60625866+Hafsah2020@users.noreply.github.com>
2021-01-18 15:09:09 +0100

    Update exercise1.dart

--- exercise1.dart
+++ exercise1.dart
@@ -4,6 +4,13 @@
  * Print out a message that tells how many years they have to be 100 years old.
  */
 
+import 'dart:io';
+
 main(List<String> args) {
-  
+  print('What\'s your name');
+  var name = stdin.readLineSync();
+  print('How old are you? ');
+  int age = int.parse(stdin.readLineSync());
+  print('Hello ${name}');
+  print('You should be 100 years old in ${100 - age} years');
 }
